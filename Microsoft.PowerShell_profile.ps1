290527cecfa3bf82b0382acd12331aab454b040c
Adam Clark <adamclerk@gmail.com>
2012-06-20 22:30:40 -0600

    final commit in preperation for June .NetDug

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+"Welcome Adam"
+""
+
+#Import-Module C:\Users\adamclerk\Documents\WindowsPowerShell\lib\Count-Object.psm1 -DisableNameChecking
+#Import-Module C:\Users\adamclerk\Documents\WindowsPowerShell\lib\Import-SecureString.psm1 -DisableNameChecking
+#Import-Module C:\Users\adamclerk\Documents\WindowsPowerShell\lib\Export-SecureString.psm1 -DisableNameChecking
+#Import-Module C:\Users\adamclerk\Documents\WindowsPowerShell\lib\Get-Weather.psm1 -DisableNameChecking
+#Import-Module C:\Users\adamclerk\Documents\WindowsPowerShell\lib\Get-GmailUnreadCount.psm1 -DisableNameChecking
+#Import-Module C:\Users\adamclerk\Documents\WindowsPowerShell\lib\Get-Map.psm1 -DisableNameChecking
+
+#$gmailpass = Import-SecureString C:\Users\adamclerk\Documents\WindowsPowerShell\pass.txt
+
+
